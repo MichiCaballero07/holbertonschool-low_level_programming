503681fa3cd98f9668901732871445b7564a49c3
MichiCaballero07 <michelcaballerogranado@gmail.com>
2023-02-20 12:44:41 -0800

    primer ejercicio

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,13 @@
+# include <stdio.h>
+# include <stdlib.h>
+# include <time.h>
+/**
+ * main - function
+ * description- if for positive or negative
+ * return:0 (success);
+ */
+
+int main(void)
+{
+	int n;
+
---
+++ variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+exercise number 0
