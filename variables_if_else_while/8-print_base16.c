bc2a9f6eccdd2ebcf9efd5f6f9b472c7f53a8f7f
MichiCaballero07 <michelcaballerogranado@gmail.com>
2023-02-21 10:49:12 -0800

    Hexadecimal

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+# include <stdio.h>
+# include <stdlib.h>
+/**
+ * main - function
+ * description - Hexadecimal
+ * Return: 0 (success);
+ */
+
+int main(void)
+{
+	int num;
+	char hexa:
+	
+	for (num = 0; num <= 9; n++)
+	putchar((num % 10) + '0');
+	for (hexa = 'a'; hexa <= 'f': hexa++)
+	putchar(hexa);
+	putchar('\n');
+	return (0);
+}
