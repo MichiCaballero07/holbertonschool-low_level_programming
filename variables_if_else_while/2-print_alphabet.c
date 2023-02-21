af89056b59de897176372f5b6ac0308d5abbc1b3
MichiCaballero07 <michelcaballerogranado@gmail.com>
2023-02-21 08:38:00 -0800

    the alphabet game

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - function
+ * description - in the alphabet
+ * Return: 0 (successs);
+ */
+int main(void)
+{
+	char mi = 'a';
+
+	for (mi = 'a'; mi <= 'z'; mi++)
+	putchar(mi);
+	putchar('\n');
+	return (0);
+}
