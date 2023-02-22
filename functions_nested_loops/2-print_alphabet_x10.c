5b4115ccc00167dd60d86f183d0754650c7c8cd3
MichiCaballero07 <michelcaballerogranado@gmail.com>
2023-02-21 19:36:43 -0800

    function that prints 10 times the alphabet

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+# include <stdio.h>
+# include "main.h"
+/**
+ *print_alphabet_x10 - function that prints 10 times the alpha.
+ *
+ */
+void print_alphabet_x10(void)
+{
+	char beta;
+	int i;
+	
+	for (i = 0; i <= 10; i++)
+	{
+		for (beta = 'a'; beta <= 'z'; beta++)
+			_putchar(beta);
+    		_putchar('\n');
+	}
+}
