f9abd98d8a20e6d95b8c9d7b4b2da0d229ebcc0f
MichiCaballero07 <michelcaballerogranado@gmail.com>
2023-02-23 19:20:55 -0800

    function that prints the numbers, from 0 to 9

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,15 @@
+# include "main.h"
+/**
+ * print_most_numbers - that prints the numbers, from 0 to 9
+ */
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		if (n != 2 && n != 4)
+			_putchar(n + '0');
+	}
+	_putchar('\n');
+}
