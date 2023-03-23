bdacf303276f4bff369c8e900f4cfd503047e1a9
MichiCaballero07 <michelcaballerogranado@gmail.com>
2023-03-23 09:59:33 -0700

    program that performs simple operations

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,76 @@
+#include <stdlib.h>
+#include "3-calc.h"
+
+
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+
+/**
+ * op_add - Returns the sum of two numbers.
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Return: The sum of a and b.
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_add - Returns the difference of two numbers.
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Return: The difference of a and b.
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_add - Returns the product of two numbers.
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Return: The product of a and b.
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+
+/**
+ * op_add - Returns the division of two numbers.
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Return: The division of a and b.
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_add - Returns the modulo of two numbers.
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Return: The modulo of a and b.
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+
+}
