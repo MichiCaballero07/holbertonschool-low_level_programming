026c5dfa4632cd09e33c48be3d367acc0b29ce89
MichiCaballero07 <michelcaballerogranado@gmail.com>
2023-04-05 16:14:30 -0700

    function that frees a dlistint_t list

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_dlistint - a function that frees a dlistint_t list
+ * @head: pointer to the first element of the list.
+ * Return: number of nodes.
+ **/
+void free_dlistint(dlistint_t *head) 
+{
+	dlistint_t *temp;
+	
+	while (head)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
