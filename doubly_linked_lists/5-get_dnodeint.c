a0a8243d80228461d2ea85a777f1ab45957a8e12
MichiCaballero07 <michelcaballerogranado@gmail.com>
2023-04-05 18:50:53 -0700

    parentesis return

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -1,19 +1,21 @@
 #include "lists.h"
-#include <stdlib.h>
-#include <string.h>
+
 /**
- * get_dnodeint_at_index - function that returns the nth node of a dlistint_t
- * @head: pointer to the first element of the list.
- * @str: char.
- * Return: number of nodes.
- **/
+ * get_dnodeint_at_index - Locates a node in a dlistint_t list.
+ * @head: The head of the dlistint_t list.
+ * @index: The node to locate.
+ *
+ * Return: If the node does not exist - NULL.
+ *         Otherwise - the address of the located node.
+ */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-unsigned int i;
-dlistint_t *current = head;
+	for (; index != 0; index--)
+	{
+		if (head == NULL)
+			return (NULL);
+		head = head->next;
+	}
 
-for (i = 0; i < index && current != NULL; i++)
-current = current->next;
-
-return (current);
+	return (head);
 }
